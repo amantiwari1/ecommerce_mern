5d20a6ea0b9306a252f022b715a160fd38bdc272
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 20:08:57 +0530

    added index including route and redux

---
+++ client/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import App from './website/App';
+import store from './shared/store';
+ReactDOM.render(
+
+  <React.StrictMode>
+    <Provider store={store} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
