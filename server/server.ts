342f2e60439aae063642eeb396d4cb9d65e91664
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 21:22:31 +0530

    start server once app.js complete

---
+++ server/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+
+
+
+const server = app.listen(app.get("server_port"), () => {
+  console.log(
+    "  App is running at %s:%d in %s mode",
+    app.get("origin_uri"),
+    app.get("server_port"),
+    app.get("env")
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
+
+export default server;
