3db1999361ee7be454953775a62d7fa0e9fe4f94
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 20:12:17 +0530

    different variables to store string in api

---
+++ client/src/shared/constants.ts
@@ -0,0 +1,17 @@
+
+
+// Change the app version value before every time you deploy your app
+// This follow the approach to force refresh the SPA suggested on:
+// https://stackoverflow.com/questions/34388614/how-to-force-update-single-page-application-spa-pages
+export const APP_VERSION: string = "0.1.0";
+
+// local storage keys
+export const ACCESS_TOKEN_KEY: string = "accessToken";
+
+export const INVALID_TOKEN_ERROR: string = "toast.user.invalid_token_error";
+
+export const RESPONSE_CONTENT_TYPE = {
+    JSON: "application/json",
+    TEXT: "text/plain",
+    HTML: "text/html"
+};
