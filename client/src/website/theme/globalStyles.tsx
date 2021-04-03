d3b0dd8ff6a056282f2a7a5beaa5487044534fbc
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 20:25:15 +0530

    added global styles

---
+++ client/src/website/theme/globalStyles.tsx
@@ -0,0 +1,25 @@
+import { Global, css } from '@emotion/react'
+
+
+import tw, { GlobalStyles as BaseStyles } from 'twin.macro'
+
+const customStyles = css`
+  * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Source Sans Pro', sans-serif;
+  }
+  body {
+    ${tw`bg-lightmode dark:(bg-darkmode text-white) transition-all`}
+  }
+` 
+
+const GlobalStyles = () => (
+  <>
+    <BaseStyles />
+    <Global styles={customStyles} />
+  </>
+)
+
+export default GlobalStyles
