8437d0636e9329345cd8716de327599ea05433e0
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 20:08:15 +0530

    added config tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          darkmode: '#0d1117',
+          lightmode: '#fff',
+        },
+      },
+    },
+    plugins: [],
+  }
