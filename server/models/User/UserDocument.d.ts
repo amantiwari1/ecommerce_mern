0497b38d5599464eb5086383886365b6e0caa3b3
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 20:22:52 +0530

    added interface types

---
+++ server/models/User/UserDocument.d.ts
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import User from "../../../client/src/models/User.d";
+export default interface UserDocument extends User, mongoose.Document {
+    comparePassword: (password: string) => Promise<Boolean>
+}
