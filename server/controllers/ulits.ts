0f3c853da8a9d34c0e1b5cb2a266f93492943f89
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 21:39:04 +0530

    added valldation handle

---
+++ server/controllers/ulits.ts
@@ -0,0 +1,16 @@
+
+import { ValidationError, Result } from "express-validator";
+import { Response } from "express";
+
+export const validationErrorResponse = (res: Response,
+                                        errors: Result<ValidationError>
+                                        ): Response | false => {
+    if (!errors.isEmpty()) {
+        const error: any /*ValidationError*/ = errors.array()[0];
+        return res
+            .status(400)
+            .json({message: error.msg});
+    } else {
+        return false;
+    }
+};
