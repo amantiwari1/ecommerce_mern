5ee7e997bae9f0e1520053b2c4e51635d97d9c6e
aman tiwari <amantiwari78632@gmail.com>
2021-04-03 20:24:43 +0530

    added new mainly page

---
+++ client/src/website/pages/Home.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+
+
+const Home = () => {
+    return (
+        <div tw="flex justify-center items-center" >
+            <h1 tw="text-5xl font-bold" >Typescript MERN Template</h1>
+        </div>
+    )
+}
+
+export default Home;
