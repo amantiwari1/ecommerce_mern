4b06b56c00b5d11badde6ebd64610bf80aebe93d
aman tiwari <amantiwari78632@gmail.com>
2021-04-05 23:03:13 +0530

    added prodoct section components

---
+++ client/src/website/components/HomeSection/ProductSection.tsx
@@ -0,0 +1,20 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+import ProductCard from '../ProductCard/ProductCard'
+
+const ProductSection = () => {
+    return (
+            <div tw="grid text-black bg-blue-300 py-10" >
+            <h1 tw=" text-4xl p-2 md:py-3 font-bold  text-center" >Curated by us, made by you.</h1>
+            <div tw="grid p-5 md:grid-cols-3 mx-auto gap-10 " >
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+            </div>
+            </div>
+    )
+}
+
+export default ProductSection
+ 
