cb33a758cb4d2d6770ad127f66da8509568619c0
aman tiwari <amantiwari78632@gmail.com>
2021-04-05 23:02:29 +0530

    added customser card components

---
+++ client/src/website/components/CustomerCard/CustomerCard.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+
+
+const CustomerCard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CustomerCard
