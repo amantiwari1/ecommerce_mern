57518947a2a7defc2f503573047f1dab237a40ab
aman tiwari <amantiwari78632@gmail.com>
2021-04-05 23:03:58 +0530

    added 2-3 components

--- client/src/website/pages/Home.tsx
+++ client/src/website/pages/Home.tsx
@@ -1,62 +1,35 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
 import tw from 'twin.macro'
-import backgroudImages from "../../assets/images/Sammee1.webp"
+import { MainSection, ProductSection , BubbleSection} from "../components"
 
 
 const Home = () => {
     return (
-        // <div tw="h-96 w-full  bg-right-top md:(h-screen bg-center)  bg-cover  py-40 px-0 " style={{ backgroundImage: `url('${backgroudImages}')` }}>
-        //     <div tw="flex flex-wrap items-center flex-row-reverse mr-4 ml-1 my-4" >
+        <div tw="flex flex-col " >
 
-        //         <div tw="flex justify-center flex-grow mb-4 max-w-full px-4" >
-        //             <div tw="pt-0 pb-16 md:pb-16 max-w-xl" >
-        //             <h1>It’s like a café in a box.</h1>
-        //             <p  >Open up to a fully curated experience with everything you need to make your favorite drinks.</p>
-        //             <button>Shop Now</button>
-        //             </div>
-        //         </div>
+            <MainSection />
 
-        //         <div tw="flex justify-center flex-grow mb-4 max-w-full px-4">
-        //                     <h1>jdsfkajsdfjdsfkajsdfjdsfkajsdfjdsfkajsdfjdsfkajs
-        //                         dfjdsfkajsdfjdsfka
-
-        //                         sfkajsdfjdsfkajsdf</h1>
-        //         </div>
-        //     </div>
-
-        // </div>
-        <div tw="relative bg-white overflow-hidden">
-            <div tw="max-w-7xl mx-auto">
-                <div tw="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-                    <main tw="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                        <div tw="sm:text-center lg:text-left">
-                            <h1 tw="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span tw="block xl:inline">Data to enrich your</span>
-                                <span tw="block text-indigo-600 xl:inline">online business</span>
-                            </h1>
-                            <p tw="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-                  </p>
-                            <div tw="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                                <div tw="rounded-md shadow">
-                                    <a href="/" tw="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                                        Get started
-                      </a>
-                                </div>
-                                <div tw="mt-3 sm:mt-0 sm:ml-3">
-                                    <a href="/" tw="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                                        Live demo
-                      </a>
-                                </div>
-                            </div>
-                        </div>
-                    </main>
-                </div>
+            <div tw="bg-indigo-500 py-32  text-center mt-96 md:mt-0  px-5  xl:px-80  2xl:px-96 " >
+                <p tw="text-white  text-3xl whitespace-pre-line" >We provide everything you need to make the best boba tea.
+                Your home might just become your favorite café! 😉</p>
             </div>
-            <div tw="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <img tw="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+
+            <ProductSection />
+            <BubbleSection />
+
+
+            <div tw="grid text-black bg-blue-300 py-10" >
+            <h1 tw=" text-4xl p-2 md:py-3 font-bold  text-center" >Curated by us, made by you.</h1>
+            <div tw="grid p-5 md:grid-cols-3 mx-auto gap-10 " >
+                  
             </div>
+            </div>
+
+
+
+
+
         </div>
 
     )
