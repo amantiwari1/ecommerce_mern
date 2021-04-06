5528ee621ebd9805244e5bc5234801cd73dd1ff5
aman tiwari <amantiwari78632@gmail.com>
2021-04-06 23:28:14 +0530

    created Quote section

---
+++ client/src/website/components/HomeSection/QuoteSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+
+const QuoteSection = () => {
+    return (
+        <div>
+             <div tw="bg-indigo-500 py-32  text-center mt-96 md:mt-0  px-5  xl:px-80  2xl:px-96 " >
+                    <p tw="text-white  text-3xl whitespace-pre-line" >We provide everything you need to make the best boba tea.
+                Your home might just become your favorite café! 😉</p>
+                </div>
+        </div>
+    )
+}
+
+export default QuoteSection
