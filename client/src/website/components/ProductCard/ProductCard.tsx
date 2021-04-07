b4e9669394e537bb5a8a65ce1dc3753f356a5b62
aman tiwari <amantiwari78632@gmail.com>
2021-04-07 22:55:48 +0530

    fixed repo image size

--- client/src/website/components/ProductCard/ProductCard.tsx
+++ client/src/website/components/ProductCard/ProductCard.tsx
@@ -5,7 +5,7 @@ import tw from 'twin.macro'
 const ProductCard = () => {
     return (
         <div tw="space-y-3 ">
-            <img tw="" src="https://cdn.shopify.com/s/files/1/0264/3066/8855/products/SAMMEE-MilkTeaKit-Classic-Reusable_384x384.jpg" alt="" /> 
+            <img tw="w-full h-auto" src="https://cdn.shopify.com/s/files/1/0264/3066/8855/products/SAMMEE-MilkTeaKit-Classic-Reusable_384x384.jpg" alt="" /> 
 
             <p tw="text-center text-3xl font-bold" >Milk Tea Powder Boba Kit</p>
 
