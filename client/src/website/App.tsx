f843c0dab12021f80ed59065c708bcdde055d4e3
aman tiwari <amantiwari78632@gmail.com>
2021-04-07 22:48:12 +0530

    added more routes

--- client/src/website/App.tsx
+++ client/src/website/App.tsx
@@ -2,7 +2,13 @@ import { ThemeProvider } from './theme/themeContext';
 import GlobalStyles from './theme/globalStyles'
 import { Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
-import {Navbar} from './components'
+import {Navbar, Footer} from './components'
+import Faq from './pages/Footer/Faq';
+import AboutUs from './pages/About/AboutUs';
+import WhatisBoba from './pages/About/WhatisBoba';
+import IngredientsList from './pages/About/IngredientsList';
+import ListofProduct from './template/ListofProduct';
+import ShoppingCart from './pages/ShoppingCart';
  
 
 function App() {
@@ -12,7 +18,14 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/"  component={Home}  />
+        <Route exact path="/cart"  component={ShoppingCart}  />
+        <Route exact path="/pages/faq"  component={Faq}  />
+        <Route exact path="/pages/about"  component={AboutUs}  />
+        <Route exact path="/pages/what-is-boba"  component={WhatisBoba}  />
+        <Route exact path="/pages/ingredients-list"  component={IngredientsList}  />
+        <Route exact path="/collections/:CollectName"  component={ListofProduct}  />
       </Switch>
+      <Footer />
     </ThemeProvider>
   );
 }
