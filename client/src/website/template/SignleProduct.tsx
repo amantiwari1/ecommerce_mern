e1afdbd62d85b04f5da868dc6fe69ab973520e0e
aman tiwari <amantiwari78632@gmail.com>
2021-04-08 20:02:11 +0530

    created sign page pages

---
+++ client/src/website/template/SignleProduct.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+
+const SignleProduct = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SignleProduct
