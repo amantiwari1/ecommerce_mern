e219a61526ef32571c07acbb172c7314b7552e8f
aman tiwari <amantiwari78632@gmail.com>
2021-04-08 20:01:35 +0530

    created sign up pages

---
+++ client/src/website/pages/SignUp.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+
+const SignUp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SignUp
