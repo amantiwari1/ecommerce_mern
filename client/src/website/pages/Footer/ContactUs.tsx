0fde313bfe13276111c0f03a2f2120e642995f5c
aman tiwari <amantiwari78632@gmail.com>
2021-04-08 20:01:47 +0530

    created contact us pages

---
+++ client/src/website/pages/Footer/ContactUs.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+
+const ContactUs = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ContactUs
