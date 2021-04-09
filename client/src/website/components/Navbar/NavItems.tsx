6c81218e707129b50fc0afeafbe6b3588f04f5cd
aman tiwari <amantiwari78632@gmail.com>
2021-04-09 23:39:11 +0530

    fixed button width full in mobile

--- client/src/website/components/Navbar/NavItems.tsx
+++ client/src/website/components/Navbar/NavItems.tsx
@@ -6,7 +6,7 @@ import { NavItemsInterface } from "./NavbarInterface"
 
 const NavItems = ({ to, children, onClick }: NavItemsInterface): JSX.Element => {
     return (
-        <Link to={to} onClick={onClick} type="button" tw="md:inline-flex block  flex-wrap py-1.5 px-4 items-center text-white justify-center rounded-xl text-center cursor-pointer transition ease-out delay-150 duration-300 hover:( bg-gray-500 ) dark:( hover:( bg-gray-600 ) text-white)" id="options-menu" aria-expanded="true" aria-haspopup="true">
+        <Link to={to} onClick={onClick} type="button" tw="md:inline-flex inline-block  flex-wrap py-1.5 px-4 items-center text-white justify-center rounded-xl text-center cursor-pointer transition ease-out delay-150 duration-300 hover:( bg-gray-500 ) dark:( hover:( bg-gray-600 ) text-white)" id="options-menu" aria-expanded="true" aria-haspopup="true">
             {children}
         </Link>
     ) 
