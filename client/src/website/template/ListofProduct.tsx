7830a1e0f7a990ff122bb9470374d693be72e943
aman tiwari <amantiwari78632@gmail.com>
2021-04-10 23:39:19 +0530

    remove xl

--- client/src/website/template/ListofProduct.tsx
+++ client/src/website/template/ListofProduct.tsx
@@ -9,7 +9,7 @@ const ListofProduct = () => {
     let { CollectName }= useParams<{ CollectName: string }>() 
     return ( <>
             <h1 tw="capitalize text-5xl font-bold text-center py-14">{CollectName.replaceAll("-", " ")}</h1>
-        <div tw="grid py-10 px-10  lg:grid-cols-4 xl:() md:(grid-cols-3) gap-8 justify-center">
+        <div tw="grid py-10 px-10  lg:grid-cols-4  md:(grid-cols-3) gap-8 justify-center">
             <ProductCard />
             <ProductCard /> 
             <ProductCard />
