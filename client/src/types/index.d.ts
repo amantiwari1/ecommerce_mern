acc9fcfdde845e6709af7903e99a3f0dfe85d083
aman tiwari <amantiwari78632@gmail.com>
2021-04-10 23:34:21 +0530

    declared rich editor due to no types

---
+++ client/src/types/index.d.ts
@@ -0,0 +1,10 @@
+declare module '@ckeditor/ckeditor5-build-classic' {
+    const ClassicEditorBuild: any;
+
+    export = ClassicEditorBuild;
+}
+declare module '@ckeditor/ckeditor5-react' {
+    const CKEditor: any;
+
+    
+}
