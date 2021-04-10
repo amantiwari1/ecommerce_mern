24a23a09dd662d99aa34ba4cba5b8e440baaa9db
aman tiwari <amantiwari78632@gmail.com>
2021-04-10 23:39:02 +0530

    styling and layout

--- client/src/website/pages/SignIn.tsx
+++ client/src/website/pages/SignIn.tsx
@@ -1,10 +1,11 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
 import tw from 'twin.macro'
-import { VscError } from 'react-icons/vsc'
 import { useForm } from 'react-hook-form'
+import { Input, FormWrapper } from "../components"
+import {Link} from 'react-router-dom'
 
-const Input = tw.input`w-full rounded-3xl focus:(ring-4  outline-none) text-black px-4 py-3`
+
 
 
 
@@ -18,65 +19,59 @@ const SignIn = () => {
 
     const onSubmit = (data: any) => {
         alert(JSON.stringify(data));
-      };
+    };
 
-    
 
-    const { register, formState: {errors}, handleSubmit  } = useForm<Inputs>()
+
+    const { register, formState: { errors }, handleSubmit } = useForm<Inputs>()
     return (
-        <div tw="flex  min-h-full h-90v justify-center items-center m-0" >
+        <FormWrapper onSubmit={handleSubmit(onSubmit)} >
 
-            <div tw="max-w-md mx-1 w-full bg-lightnav text-white overflow-hidden rounded-lg shadow-2xl  dark:(bg-darknav) px-10" >
-
-                <form tw="max-w-full w-80 mx-auto py-8" onSubmit={handleSubmit(onSubmit)} >
-
-                    
- 
-                    <div tw=" mb-3 pb-5 relative" >
-                        <label tw="inline-block mb-1 ml-2" >Email</label>
-
-                        <Input placeholder="Enter the Email Address" css={[errors.email ? tw`ring ring-red-400  mb-0 ` : tw`ring-blue-400 mb-7`]} {...register("email", {
-                            required: 'You must provide the email address!',
-                            pattern: {
-                                // eslint-disable-next-line
-                                value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                message: 'Invalid email address',
-                            },
-                        })}  />
-
-                        
-
-                        {
-                            errors.email && <> <VscError tw="absolute h-6 w-6 top-10 right-2 text-red-500" /> <p tw="ml-2 text-red-500 mt-1 " >{errors.email.message}</p> </>
-                        }
-
-                    </div>
-                    <div tw=" mb-3 pb-5 relative" >
-                        <label tw="inline-block mb-1 ml-2" >Password</label>
-
-                        <Input placeholder="Enter the password" css={[errors.password ? tw`ring ring-red-400   mb-0 ` : tw`ring-blue-400 mb-7`]} type="password" {...register("password", {
-                            required: 'You must provide a password.',
-                            minLength: {
-                                value: 6,
-                                message: 'Your password must be greater than 6 ', 
-                            }
-                        })} />
-
-                        
-                        {
-                            errors.password && <> <VscError tw="absolute h-6 w-6 top-10 right-2 text-red-500" /> <p tw="ml-2 text-red-400 mt-1 " >{errors.password.message}</p> </>
-                        }
-
-                    </div>
-                    <div tw=" flex mb-3 pb-5 justify-center " >
-                        <button type="submit"  tw="py-3  focus:outline-none w-40 text-white text-center bg-indigo-600 hover:bg-indigo-500 rounded-full " >Sign In</button>
-
-                    </div>
-
-                </form>
+            <div tw=" flex mb-3 pb-5 justify-center " >
+                <h1 tw="font-bold text-2xl md:text-3xl" >Welcome to Sammee</h1>
             </div>
 
-        </div>
+            <Input 
+            isError={errors.email} 
+            label="Email" 
+            placeholder="Enter your email"
+            register={register("email", {
+                required: 'You must provide the email address!',
+                pattern: {
+                    // eslint-disable-next-line
+                    value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: 'Invalid email address',
+                },
+            })} />
+
+            <Input 
+            type="password" 
+            isError={errors.password} 
+            label="Password"
+            placeholder="Enter your Password"
+            register={register("password", {
+                required: 'You must provide a password.',
+                minLength: {
+                    value: 6,
+                    message: 'Your password must be greater than 6 ',
+                }
+            })} />
+
+
+            <div tw=" flex mb-3 pb-5 justify-center " >
+                <button type="submit" tw="py-3  focus:outline-none w-40 text-white text-center bg-indigo-600 hover:bg-indigo-500 rounded-full " >Sign In</button>
+            </div>
+           
+            <div tw=" flex mb-3 pb-5 justify-center " >
+                <p>
+                    Don't have account? 
+                <Link to="/signup" tw="ml-1  focus:outline-none   text-center text-indigo-600 hover:text-indigo-500 rounded-full " >Create account</Link>
+                </p>
+            </div>
+
+
+
+        </ FormWrapper >
     )
 }
 
--- client/src/website/pages/SignUp.tsx
+++ client/src/website/pages/SignUp.tsx
@@ -1,119 +1,100 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
 import tw from 'twin.macro'
-import { VscError } from 'react-icons/vsc'
 import { useForm } from 'react-hook-form'
-
-const Input = tw.input`w-full rounded-3xl focus:(ring-4  outline-none) text-black px-4 py-3`
-
-
+import { Input, FormWrapper } from "../components"
+import { Link } from 'react-router-dom'
+import { useRef } from 'react';
 
 
 type Inputs = {
     email: string,
-    name:string,
+    name: string,
     password: string,
+    confirmpassword: string,
 };
 
 const SignUp = () => {
 
+
+
+
+    const { register, watch, formState: { errors }, handleSubmit } = useForm<Inputs>()
+
     const onSubmit = (data: any) => {
+
+
         alert(JSON.stringify(data));
-      };
+    };
 
-    
+    const password = useRef({});
+    password.current = watch("password", "");
 
-    const { register, formState: {errors}, handleSubmit  } = useForm<Inputs>()
+
     return (
-        <div tw="flex  min-h-full h-90v justify-center items-center m-0" >
+        <FormWrapper onSubmit={handleSubmit(onSubmit)} >
 
-            <div tw="max-w-md mx-1 w-full bg-lightnav text-white overflow-hidden rounded-lg shadow-2xl  dark:(bg-darknav) px-10" >
-
-                <form tw="max-w-full w-80 mx-auto py-8" onSubmit={handleSubmit(onSubmit)} >
-
-                    
- 
-                    <div tw=" mb-3 pb-5 relative" >
-                        <label tw="inline-block mb-1 ml-2" >Name</label> 
-
-                        <Input placeholder="Enter the Name" css={[errors.name ? tw`ring ring-red-400  mb-0 ` : tw`ring-blue-400 mb-7`]} {...register("name", {
-                            required: 'You must provide the name',
-                        })}  />
-
-                        
-
-                        {
-                            errors.name && <> <VscError tw="absolute h-6 w-6 top-10 right-2 text-red-500" /> <p tw="ml-2 text-red-500 mt-1 " >{errors.name.message}</p> </>
-                        }
-
-                    </div>
-
-                    <div tw=" mb-3 pb-5 relative" >
-                        <label tw="inline-block mb-1 ml-2" >Email</label>
-
-                        <Input placeholder="Enter the Email Address" css={[errors.email ? tw`ring ring-red-400  mb-0 ` : tw`ring-blue-400 mb-7`]} {...register("email", {
-                            required: 'You must provide the email address!',
-                            pattern: {
-                                // eslint-disable-next-line
-                                value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                message: 'Invalid email address',
-                            },
-                        })}  />
-
-                        
-
-                        {
-                            errors.email && <> <VscError tw="absolute h-6 w-6 top-10 right-2 text-red-500" /> <p tw="ml-2 text-red-500 mt-1 " >{errors.email.message}</p> </>
-                        }
-
-                    </div>
+            <div tw=" flex mb-3 pb-5 justify-center " >
+                <h1 tw="font-bold text-3xl" >Create Account</h1>
+            </div>
+            <Input
+                isError={errors.name}
+                placeholder="Enter your Full Name"
+                label="Name"
+                register={register("name", {
+                    required: 'You must provide the name',
+                })} />
 
 
+            <Input
+                placeholder="Enter your email"
+                isError={errors.email}
+                label="Email"
+                register={register("email", {
+                    required: 'You must provide the email address!',
+                    pattern: {
+                        // eslint-disable-next-line
+                        value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: 'Invalid email address',
+                    },
+                })} />
 
-                    <div tw=" mb-3 pb-5 relative" >
-                        <label tw="inline-block mb-1 ml-2" >Password</label>
+            <Input
+                placeholder="Enter your Password"
+                type="password"
+                isError={errors.password}
+                label="Password"
+                register={register("password", {
+                    required: 'You must provide a password.',
+                    minLength: {
+                        value: 6,
+                        message: 'Password must have at least 6 characters',
+                    }
+                })} />
 
-                        <Input placeholder="Enter the password" css={[errors.password ? tw`ring ring-red-400   mb-0 ` : tw`ring-blue-400 mb-7`]} type="password" {...register("password", {
-                            required: 'You must provide a password.',
-                            minLength: {
-                                value: 6,
-                                message: 'Your password must be greater than 6 ', 
-                            }
-                        })} />
+            <Input
+                placeholder="Enter your confirm password"
+                type="password"
+                isError={errors.confirmpassword}
+                label="Confirm Password"
+                register={register("confirmpassword", {
+                    required: 'You must provide a confirm password.',
+                    validate: value =>
+                        value === password.current || "The passwords do not match"
+                })} />
 
-                        
-                        {
-                            errors.password && <> <VscError tw="absolute h-6 w-6 top-10 right-2 text-red-500" /> <p tw="ml-2 text-red-500 mt-1 " >{errors.password.message}</p> </>
-                        }
-
-                    </div>
-                    
-                    <div tw=" mb-3 pb-5 relative" >
-                        <label tw="inline-block mb-1 ml-2" >Password</label>
-
-                        <Input placeholder="Enter the password" css={[errors.password ? tw`ring ring-red-400   mb-0 ` : tw`ring-blue-400 mb-7`]} type="password" {...register("password", {
-                            required: 'You must provide a password.',
-                            minLength: {
-                                value: 6,
-                                message: 'Your password must be greater than 6 ', 
-                            }
-                        })} />
-
-                        
-                        {
-                            errors.password && <> <VscError tw="absolute h-6 w-6 top-10 right-2 text-red-500" /> <p tw="ml-2 text-red-500 mt-1 " >{errors.password.message}</p> </>
-                        }
-
-                    </div>
-                    <div tw=" flex mb-3 pb-5 justify-center " >
-                        <button type="submit"  tw="py-3  focus:outline-none w-40 text-white text-center bg-indigo-600 hover:bg-indigo-500 rounded-full " >Sign Up</button>
-
-                    </div>
-
-                </form>
+            <div tw=" flex mb-3 pb-5 justify-center " >
+                <button type="submit" tw="py-3  focus:outline-none w-40 text-white text-center bg-indigo-600 hover:bg-indigo-500 rounded-full " >Sign Up</button>
             </div>
 
-        </div>
+            <div tw=" flex mb-3 pb-5 justify-center " >
+                <p>
+                    have already account?
+                <Link to="/signin" tw="ml-1  focus:outline-none   text-center text-indigo-600 hover:text-indigo-500 rounded-full " >Sign In</Link>
+                </p>
+            </div>
+
+        </ FormWrapper >
     )
 }
 
