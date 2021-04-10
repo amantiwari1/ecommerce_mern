c1cffae6c0a1650e12f2640ee69435ff21803e6f
aman tiwari <amantiwari78632@gmail.com>
2021-04-10 23:30:27 +0530

    created Product Interace

---
+++ client/src/models/Product.d.ts
@@ -0,0 +1,9 @@
+import { UnifiedModel } from "./UnifiedModel";
+export default interface Product extends UnifiedModel {
+    title: string;
+    description: string;
+    price: string;
+    featureImage: string;
+    ImageArray: string[];
+    category: string;
+}
