598fa6ecb80ea7dcddacfaf5c962867c959b7988
aman tiwari <amantiwari78632@gmail.com>
2021-04-11 22:51:47 +0530

    added props

--- client/src/website/components/ProductCard/ProductCard.tsx
+++ client/src/website/components/ProductCard/ProductCard.tsx
@@ -1,15 +1,16 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
 import tw from 'twin.macro'
+import ProductCardInterface from './ProductCardInterface'
 
-const ProductCard = () => {
+const ProductCard = ( { title, price, img }: ProductCardInterface )  => {
     return (
         <div tw="space-y-3 ">
-            <img tw="w-full h-auto" src="https://cdn.shopify.com/s/files/1/0264/3066/8855/products/SAMMEE-MilkTeaKit-Classic-Reusable_384x384.jpg" alt="" /> 
+            <img tw="w-full h-auto" src={img} alt="" /> 
 
-            <p tw="text-center text-3xl font-bold" >Milk Tea Powder Boba Kit</p>
+            <p tw="text-center text-3xl font-bold" >{title}</p>
 
-            <p tw="text-center text-2xl " >from $25.00</p>
+            <p tw="text-center text-2xl " >from ${price}</p>
             
         </div>
     )
