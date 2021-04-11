efa48390ddde6d1e8a9bb132ebcda25681d69cf9
aman tiwari <amantiwari78632@gmail.com>
2021-04-11 22:52:21 +0530

    add get product function

--- client/src/website/template/ListofProduct.tsx
+++ client/src/website/template/ListofProduct.tsx
@@ -3,20 +3,35 @@
 import tw from 'twin.macro'
 import { useParams } from 'react-router'
 import { ProductCard } from '../components'
+import { useEffect } from 'react'
+import productActionCreator from '../../actions/productAction'
+import { useAppDispatch, useAppSelector } from '../../shared/reduxHooks'
+
 
 const ListofProduct = () => {
 
-    let { CollectName }= useParams<{ CollectName: string }>() 
-    return ( <>
-            <h1 tw="capitalize text-5xl font-bold text-center py-14">{CollectName.replaceAll("-", " ")}</h1>
+    let { CollectName } = useParams<{ CollectName: string }>()
+    const AllProduct = useAppSelector((state) => state.productReducer)
+  const dispatch = useAppDispatch()
+    
+
+    useEffect(() => {
+        dispatch(productActionCreator.getProducts())
+    }, [dispatch])
+
+
+    return (<>
+        <h1 tw="capitalize text-5xl font-bold text-center py-14">{CollectName.replaceAll("-", " ")}</h1>
         <div tw="grid py-10 px-10  lg:grid-cols-4  md:(grid-cols-3) gap-8 justify-center">
-            <ProductCard />
-            <ProductCard /> 
-            <ProductCard />
-            <ProductCard /> 
-            
+        
+        {
+            AllProduct.data.map(product => (
+                <ProductCard  price={product.price} title={product.title} img={product.featureImage} />
+            ))
+        }
+
         </div>
-        </>
+    </>
     )
 }
 
