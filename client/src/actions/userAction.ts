bc889e60982141f631874b9d2c3b4081982952d4
aman tiwari <amantiwari78632@gmail.com>
2021-04-12 23:04:39 +0530

    added user action and slice

---
+++ client/src/actions/userAction.ts
@@ -0,0 +1,15 @@
+import { Dispatch } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+import ProductState from "../models/client/PorudactState";
+
+
+
+
+const BASE_URL = "http://localhost:3001";
+
+const userActionCreator = {
+    createUser:  (post: any) => async (dispatch: Dispatch) => {
+        
+    }
+}
---
+++ client/src/reducers/UserSlice.ts
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+import  UserState from "../models/client/UserState";
+
+
+const initialState: UserState = {
+    currentUser: 
+}
