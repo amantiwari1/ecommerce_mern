6277d1a08b4039866ac439429cf938cf0aac9e5a
aman tiwari <amantiwari78632@gmail.com>
2021-04-12 23:05:46 +0530

    added get proudct function

--- client/src/reducers/ProductSlice.ts
+++ client/src/reducers/ProductSlice.ts
@@ -1,30 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit'
-import ProductState from '../models/client/PorudactState'
-  
-  // Define the initial state using that type
-  const initialState: ProductState = {
-    loading: false,
-    valid: false,
-    data: [],
-  }
+import { createSlice } from "@reduxjs/toolkit";
+import ProductState from "../models/client/PorudactState";
+
+// Define the initial state using that type
+const initialState: ProductState = {
+  loading: false,
+  valid: false,
+  data: [],
+  SingleProduct: {
+    title: "",
+    ImageArray: [],
+    category: "",
+    description: "",
+    featureImage: "",
+    price: "",
+    titleslug: "",
+  },
+};
 
 export const productSlice = createSlice({
-  name:'product',
+  name: "product",
   initialState: initialState,
   reducers: {
-    SaveProductSuccess: (state, action) => { 
-      return { loading: false, valid: false , data: [...state.data, {...action.payload}]} 
+    SaveProductSuccess: (state, action) => {
+      return {
+        ...state,
+        loading: false,
+        valid: false,
+        data: [...state.data, { ...action.payload }],
+      };
     },
     ProductBegin: (state) => {
-      return { ...state, loading: true, valid: true } 
+      return { ...state, loading: true, valid: true };
     },
     GetProductsSuccess: (state, action) => {
-      return {loading: false, valid:false, data: action.payload} 
-    }
-  }
-}) 
-  
-export const { SaveProductSuccess, ProductBegin, GetProductsSuccess } = productSlice.actions
+      return { ...state, loading: false, valid: false, data: action.payload };
+    },
+    GetSingleProductSuccess: (state, action) => {
+      return {
+        ...state,
+        loading: false,
+        valid: false,
+        SingleProduct: { ...action.payload },
+      };
+    },
+  },
+});
 
-export default productSlice.reducer
-  
+export const {
+  SaveProductSuccess,
+  ProductBegin,
+  GetProductsSuccess,
+  GetSingleProductSuccess,
+} = productSlice.actions;
+
+export default productSlice.reducer;
