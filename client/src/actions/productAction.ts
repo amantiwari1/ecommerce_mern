9486ec691edfd58353d23389e770a56d135d1ba9
aman tiwari <amantiwari78632@gmail.com>
2021-04-12 23:03:59 +0530

    fix error handle and added get product

--- client/src/actions/productAction.ts
+++ client/src/actions/productAction.ts
@@ -1,18 +1,28 @@
 import axios from "axios";
-import { SaveProductSuccess, ProductBegin, GetProductsSuccess } from "../reducers/ProductSlice";
+import { SaveProductSuccess, ProductBegin, GetProductsSuccess , GetSingleProductSuccess} from "../reducers/ProductSlice";
 import { Dispatch } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const BASE_URL = "http://localhost:3001";
 
 const productActionCreator = {
   createProduct: (post: any) => async (dispatch: Dispatch) => {
-    try {
+    
       dispatch(ProductBegin());
-      const result = await axios.post(`${BASE_URL}/product/create`, post);
-      dispatch(SaveProductSuccess(result));
-    } catch (error) {
-      console.error(error);
-    }
+
+      
+      
+        await axios.post(`${BASE_URL}/product/create`, post) 
+        .then(res => {  
+          toast.success("Successfully")
+          dispatch(SaveProductSuccess(res.data))  
+        })    
+        .catch(err => {  
+          toast.error(err.response.data.message)
+        }); 
+     
+  
+  
   },
 
   getProducts: () => async (dispatch: Dispatch) => {
@@ -23,7 +33,27 @@ const productActionCreator = {
     } catch (error) {
       console.error(error);
     }
+  },
+  getCategoryProducts: (category: string) => async (dispatch: Dispatch) => {
+    try {
+      dispatch(ProductBegin());
+      const AllProducts = await axios.get(`${BASE_URL}/product/getsCategories?category=${category}`)
+      dispatch(GetProductsSuccess(AllProducts.data))
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  getProduct: (titleslug: string) => async (dispatch: Dispatch) => {
+    await axios.get(`${BASE_URL}/product/get/${titleslug}`) 
+    .then(res => {  
+      dispatch(GetSingleProductSuccess(res.data))  
+    })    
+    .catch(err => {  
+      toast.error(err.response.data.message) 
+    }); 
   }
+
+
 };
 
 export default productActionCreator;
