9b3c93d8e5c7340b47a5ad91747d3e7d4194b64b
aman tiwari <amantiwari78632@gmail.com>
2021-04-14 23:59:58 +0530

    added protected routes

--- server/routes/product.routes.ts
+++ server/routes/product.routes.ts
@@ -2,6 +2,7 @@ import * as productController from "../controllers/product.controller";
 import { Router } from "express";
 import { check } from "express-validator";
 import {use} from "../util/TryCatch"
+import passport from "passport";
 
 const updateProductValidations = [
   check("title", "toast.post.title_empty").not().isEmpty(),
@@ -10,8 +11,8 @@ const updateProductValidations = [
 const product: Router = Router();
 
 product.post("/create", updateProductValidations, use(productController.create)  );
-product.get("/gets", use(productController.getProducts));
+product.get("/gets",passport.authenticate('jwt', { session: false }) , use(productController.getProducts));
 product.get("/getsCategories", use(productController.getProductCategory));
 product.get("/get/:titleslug", use(productController.getProduct));
 
-export default product;
+export default product; 
