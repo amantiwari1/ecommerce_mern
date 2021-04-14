024ae220186da6364db91b69b172e578deb0320d
aman tiwari <amantiwari78632@gmail.com>
2021-04-14 23:59:06 +0530

    fixed error handlles

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as authController from "../controllers/auth.controller";
+import {use} from "../util/TryCatch"
 
 const auth = Router();
 
-auth.post("/signup", authController.signUp);
-auth.post("/signin", authController.signIn);
+auth.post("/signup", use(authController.signUp));
+auth.post("/signin", use(authController.signIn));
 
 export default auth;
