679f3fcdf2821b1f83e2d96e9eb9861a2cd737f3
aman tiwari <amantiwari78632@gmail.com>
2021-04-14 23:58:21 +0530

    added httponly cookie passport middleware

--- server/middlewares/passport.ts
+++ server/middlewares/passport.ts
@@ -1,20 +1,45 @@
-import { Strategy, ExtractJwt, StrategyOptions } from "passport-jwt";
-import {JWT_SECRET} from "../util/secrets";
+import {
+  JwtFromRequestFunction,
+  Strategy,
+  StrategyOptions,
+} from "passport-jwt";
+import { NextFunction, Request } from "express";
+import { JWT_SECRET } from "../util/secrets";
 import UserCollection from "../models/User/UserCollection";
 
+const cookieExtractor: JwtFromRequestFunction = (req: Request) => {
+  let jwt = null;
+
+  if (req && req.cookies) {
+    jwt = req.cookies["jwt"];
+
+    if (!jwt) {
+      throw new Error("Not Authenticated");
+    }
+  }
+  return jwt;
+};
+
 const opts: StrategyOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  jwtFromRequest: cookieExtractor,
   secretOrKey: JWT_SECRET,
 };
+
 export default new Strategy(opts, async (payload, done) => {
   const user = await UserCollection.findById(payload.id);
 
   try {
     if (user) {
-      return done(undefined, user);
+      const { expiration } = payload;
+
+      if (Date.now() > expiration) {
+        done("Unauthorized", false);
+      }
+
+      return done(undefined, { id: user._id, email: user.email });
     }
 
-    return done(undefined, false);
+    return done("Unauthorized", false);
   } catch (error) {
     console.log(error);
   }
