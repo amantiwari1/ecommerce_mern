201750a863ecd7a3bd74dcbcfca6d7077dfea8c8
aman tiwari <amantiwari78632@gmail.com>
2021-04-15 23:51:09 +0530

    added protected routes in client side

--- client/src/website/App.tsx
+++ client/src/website/App.tsx
@@ -22,7 +22,7 @@ import { Navbar, Footer } from './components'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify'
 import { useEffect } from 'react';
-import { useAppDispatch } from '../shared/reduxHooks';
+import { useAppDispatch, useAppSelector } from '../shared/reduxHooks';
 import userActionCreator from '../actions/userAction';
 
 
@@ -30,6 +30,7 @@ import userActionCreator from '../actions/userAction';
 function App() {
 
     const dispatch = useAppDispatch()
+    const isAdmin = useAppSelector(state => state.userReducer.currentUser.isAdmin)
 
    useEffect(() => {
     dispatch(userActionCreator.isLogin())
@@ -43,18 +44,22 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/cart" component={ShoppingCart} />
-        <Route exact path="/editProduct" component={AddProduct} />
         <Route exact path="/ContactUs" component={ContactUs} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/collections/:CollectName/product/:productName" component={SingleProduct} />
         <Route exact path="/pages/" component={ShoppingCart} />
-        <Route exact path="/pages/faq" component={Faq} />
         <Route exact path="/pages/about" component={AboutUs} />
         <Route exact path="/pages/what-is-boba" component={WhatisBoba} />
         <Route exact path="/pages/ingredients-list" component={IngredientsList} />
         <Route exact path="/collections/:CollectName" component={ListofProduct} />
-      </Switch>
+        <Route exact path="/pages/faq" component={Faq} />
+        
+        {
+          isAdmin &&
+          <Route exact path="/editProduct" component={AddProduct} />
+        }
+      </Switch> 
       <Footer />
       <ToastContainer
         position="bottom-right"
