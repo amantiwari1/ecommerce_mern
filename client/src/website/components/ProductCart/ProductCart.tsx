c26cd0838d7fe0843b9a2a921119392d9f7b22ee
aman tiwari <amantiwari78632@gmail.com>
2021-04-16 23:56:06 +0530

    added props to Products componentss

--- client/src/website/components/ProductCart/ProductCart.tsx
+++ client/src/website/components/ProductCart/ProductCart.tsx
@@ -1,22 +1,23 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
 import tw from 'twin.macro'
+import ProductCartInterface from './ProductInterface'
 
-const ProductCart = () => {
+const ProductCart = ( {quality, title, featureImage, price} : ProductCartInterface )  => {
     return (
         <div tw="grid  grid-cols-4 gap-5 md:(grid-cols-6 gap-2) py-5 md:items-center ">
             <div tw=" flex col-span-4 md:col-span-3 space-x-7" >
-                <img tw="h-24 w-24  " src="https://cdn.shopify.com/s/files/1/0264/3066/8855/products/SAMMEE-Yogurt-Kit_768x768.jpg" alt="" />
+                <img tw="h-24 w-24  " src={featureImage} alt="" />
                 <div >
-                    <p>Yogurt Smoothie Boba Kit</p>
-                    <p>6 Plastic Boba Straws</p>
-                    <button>Remove</button>
+                    <p>{title}</p>
+                    
+                    <button>Remove</button> 
                 </div>
             </div>
 
             <div tw="flex col-span-4  justify-end md:(col-span-1 justify-start) space-x-2" >
                 <p tw=" md:hidden ">Price :</p>
-                <p tw=" " >$25.00</p>
+                <p tw=" " >${price}</p>
             </div>
 
 
@@ -24,7 +25,7 @@ const ProductCart = () => {
                 <div tw="flex space-x-2" >
                     <button tw="font-bold border px-2" >+</button>
 
-                    <p >5</p>
+                    <p >{quality}</p>
 
                     <button tw="font-bold border px-3" >-</button>
                 </div>
