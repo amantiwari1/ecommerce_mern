187ee0bb13d1e7325cc79714aa27f295f3885538
aman tiwari <amantiwari78632@gmail.com>
2021-04-17 23:48:14 +0530

    added update function in products

--- server/controllers/product.controller.ts
+++ server/controllers/product.controller.ts
@@ -4,85 +4,96 @@ import ProductCollection from "../models/Product/ProductCollection";
 import { validationErrorResponse } from "./ulits";
 import { validationResult } from "express-validator";
 
+export const create: RequestHandler = async (req: Request, res: Response) => {
+  const invalid: Response | false = validationErrorResponse(
+    res,
+    validationResult(req)
+  );
+  if (invalid) {
+    return invalid;
+  }
 
-export const create: RequestHandler = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+  const titleslug = req.body.titleslug;
 
+  const isExistTitleSlug = await ProductCollection.exists({
+    titleslug: titleslug,
+  });
 
-    const invalid: Response | false = validationErrorResponse(
-      res,
-      validationResult(req)
-    );
-    if (invalid) {
-      return invalid;
-    }
-  
-    const titleslug  = req.body.titleslug
-  
-    const isExistTitleSlug = await ProductCollection.exists({titleslug: titleslug})
-  
-  
-    
-    if (isExistTitleSlug) {
-      throw new Error("Already Title Exists");
-    }
-    
-    
-  
-    const product: ProductDocument = new ProductCollection({
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      featureImage: req.body.featureImage,
-      ImageArray: req.body.ImageArray,
-      category: req.body.category,
-      titleslug: titleslug,
-    });
-      const saved = await product.save();
-      return res.status(201).json(saved);
-    
-  } 
-  
+  if (isExistTitleSlug) {
+    throw new Error("Already Title Exists");
+  }
 
+  const product: ProductDocument = new ProductCollection({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    featureImage: req.body.featureImage,
+    ImageArray: req.body.ImageArray,
+    category: req.body.category,
+    titleslug: titleslug,
+  });
+  const saved = await product.save();
+  return res.status(201).json(saved);
+};
 
 export const getProducts: RequestHandler = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-    const AllProduct = await ProductCollection.find();
-    return res.status(200).json(AllProduct);
-
+  const AllProduct = await ProductCollection.find();
+  return res.status(200).json(AllProduct);
 };
 
 export const getProductCategory: RequestHandler = async (
   req: Request,
-  res: Response, 
+  res: Response,
   next: NextFunction
 ) => {
-    const category = req.query.category
-    const AllProductCategory = await ProductCollection.find().where({ category: category})
-    return res.status(200).json(AllProductCategory)
-}
+  const category = req.query.category;
+  const AllProductCategory = await ProductCollection.find().where({
+    category: category,
+  });
+  return res.status(200).json(AllProductCategory);
+};
 export const getProduct: RequestHandler = async (
   req: Request,
-  res: Response, 
-  next: NextFunction
+  res: Response
 ) => {
-    const titleslug = req.params.titleslug
+  const titleslug = req.params.titleslug;
 
-    const isExistTitleSlug = await ProductCollection.exists({titleslug: titleslug})
-  
-  
-    
-    if (!isExistTitleSlug) {
-      throw new Error("not found");
-    }
-    
+  const isExistTitleSlug = await ProductCollection.exists({
+    titleslug: titleslug,
+  });
 
-    const Product = await ProductCollection.findOne({ titleslug: titleslug})
-    return res.status(200).json(Product)
-}
+  if (!isExistTitleSlug) {
+    throw new Error("not found");
+  }
+
+  const Product = await ProductCollection.findOne({ titleslug: titleslug });
+  return res.status(200).json(Product);
+};
+
+export const updateProduct: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+
+  
+  const updated = await ProductCollection.findByIdAndUpdate(
+    { _id: req.body._id },
+    {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        featureImage: req.body.featureImage,
+        ImageArray: req.body.ImageArray,
+        category: req.body.category,
+      },
+  ).exec();
+
+  return res.status(200).json({msg: "updated" });
+
+
+
+};
