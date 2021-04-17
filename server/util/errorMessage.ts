7e4cd92f88a2ae68f1aeccb33abe6824b7e833a8
aman tiwari <amantiwari78632@gmail.com>
2021-04-17 23:49:44 +0530

    added new util functions

---
+++ server/util/errorMessage.ts
@@ -0,0 +1,19 @@
+const Errors = (message: string, statusCode: number) => {
+  const error = new Error(message);
+
+  error.statusCode = statusCode;
+  throw error;
+};
+
+const errorMessage = {
+  unauthorized: () => {
+    Errors("Not authorized", 401)
+  },
+  invalid: (message?: string) => {
+    Errors( message || "Invalid credentials", 422)
+  },
+
+};
+
+
+export default errorMessage;
