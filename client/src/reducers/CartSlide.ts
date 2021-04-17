8df267af0c1337b9f4a8d14c90b4c91a4bd18647
aman tiwari <amantiwari78632@gmail.com>
2021-04-17 23:42:38 +0530

    added remove and update reducer functions

--- client/src/reducers/CartSlide.ts
+++ client/src/reducers/CartSlide.ts
@@ -3,7 +3,8 @@ import  CartState from "../models/client/CartState";
 
 
 const initialState: CartState = {
-    data: []
+    data: [],
+    fullTotal: 0
 }
 
 const cartSlice = createSlice({
@@ -12,15 +13,35 @@ const cartSlice = createSlice({
 
     reducers: {
         getsCartData: (state, action) => {
+            let totalPrice = action.payload.reduce(function (accumulator: any, item: any) {
+                return accumulator + item.total;
+              }, 0);
             state.data = action.payload 
+            state.fullTotal = totalPrice
+        },
+        updateCartData: (state, action) => {
+            for (let cart of state.data) {
+                if (cart.id === action.payload.id) {
+                    cart.quality += action.payload.value
+                    cart.total = cart.quality*cart.price
+                    state.fullTotal += (cart.price*action.payload.value)
+                }
+            }
+        },
+        removeCartData: (state, action) => {
+            let updatedCart = state.data.filter(arr => arr.id !== action.payload )
+            state.data = updatedCart
         } 
+
     }
 
 })
 
 
 export const {
-    getsCartData
+    getsCartData,
+    updateCartData,
+    removeCartData
   } = cartSlice.actions;
   
   export default cartSlice.reducer;
