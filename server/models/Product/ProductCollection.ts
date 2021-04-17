f4684c23546a783743a37091b156cbab0fa447ca
aman tiwari <amantiwari78632@gmail.com>
2021-04-17 23:49:02 +0530

    fixed wrong data type in models

--- server/models/Product/ProductCollection.ts
+++ server/models/Product/ProductCollection.ts
@@ -6,7 +6,7 @@ export const ProductSchema = new Schema<ProductDocument>(
         title: { type: String, required: true },
         titleslug: { type: String, required: true, unique: true},
         description: { type: String, required: true},
-        price: { type: String, required: true},
+        price: { type: Number, required: true},
         featureImage: { type: String, required: true},
         ImageArray: { type: Array, required: true},
         category: { type: String, required: true},
