bf891b7b701af8551252d5db581817d1a901681b
aman tiwari <amantiwari78632@gmail.com>
2021-04-17 23:47:42 +0530

    added fully  update, remove and create in cart

--- server/controllers/cart.controller.ts
+++ server/controllers/cart.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import UserCollection from "../models/User/UserCollection";
 import ProductCollection from "../models/Product/ProductCollection";
+import errorMessage from "../util/errorMessage";
 
 export interface IGetUserAuthInfoRequest extends Request {
   user?: {
@@ -13,47 +14,62 @@ export interface IGetUserAuthInfoRequest extends Request {
 
 export const read: RequestHandler = async (
   req: IGetUserAuthInfoRequest,
-  res: Response,
-  next: NextFunction
+  res: Response
 ) => {
   if (req.user && req.user.cart) {
-    
-    const cart = await Promise.all(req.user.cart.map(
-        async (product: any) => {
-
-            const oneCart = await ProductCollection.findById(product.ProductId)
-            if (oneCart) {
-                return {
-                    id: product.ProductId,
-                    quality: product.Quality,
-                    title: oneCart.title,
-                    titleslug: oneCart.titleslug,
-                    featureImage: oneCart.featureImage,
-                    price: oneCart.price
-                };
-            }
+    const cart = await Promise.all(
+      req.user.cart.map(async (product: any) => {
+        const oneCart = await ProductCollection.findById(product.ProductId);
+        if (oneCart) {
+          return {
+            id: product.ProductId,
+            quality: product.Quality,
+            title: oneCart.title,
+            titleslug: oneCart.titleslug,
+            featureImage: oneCart.featureImage,
+            price: oneCart.price,
+            total: product.Quality * oneCart.price,
+          };
         }
-    ))
-    
+      })
+    );
+
     return res.status(200).json({ cart: cart });
   }
 };
 
-
-
 export const add: RequestHandler = async (
   req: IGetUserAuthInfoRequest,
-  res: Response,
-  next: NextFunction
+  res: Response
 ) => {
   const user = await UserCollection.findById(req.user!.id).exec();
+  const isExistCart = await UserCollection.findOneAndUpdate(
+    { _id: req.user!.id, "cart.ProductId": req.body.ProductId },
+    {
+      $inc: {
+        "cart.$.Quality": req.body.Quality,
+      },
+    }
+  );
 
   if (user) {
-    user.cart = [...user.cart, req.body];
+    if (!isExistCart) {
+      user.cart = [...user.cart, req.body];
+      user.save();
+    }
 
-    console.log(req.body);
-
-    user.save();
     return res.status(201).json({ cart: req.body });
   }
+};
+
+export const remove: RequestHandler = async (req: IGetUserAuthInfoRequest, res: Response) => {
+  if (!req.params.id) {
+    errorMessage.invalid("invalid id");
+  }
+
+   UserCollection.updateOne({
+    _id: req.user!.id,
+  }, {"$pull": { "cart": {"ProductId":req.params.id }}}).exec();
+
+  return res.status(201).json({ msg: "deleted"})
 };
