6e582a0141e7b346ac007501f6732c37a3d91218
aman tiwari <amantiwari78632@gmail.com>
2021-04-18 23:52:11 +0530

    input file and formData

--- client/src/website/pages/AddProduct.tsx
+++ client/src/website/pages/AddProduct.tsx
@@ -9,7 +9,7 @@ import queryString from 'query-string'
 
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { useLocation } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 
 const Input = tw.input`w-full  border   text-center text-black text-3xl focus:(outline-none ring-4)`
@@ -18,16 +18,48 @@ const Label = tw.label` font-bold m-4`
 const AddProduct = () => {
   const { search } = useLocation()
   const values = queryString.parse(search)
+  const history = useHistory()
 
   const { register, handleSubmit, setValue, formState: { errors }, getValues } = useForm<Product>()
   const dispatch = useAppDispatch()
+
   const onSubmit = (data: any) => {
+
+
+
+
+
     try {
-      dispatch(productActionCreator.createProduct({
-        ...data, ImageArray: data.ImageArray.split('\n'),
-        titleslug: data.title.toLowerCase().replace(/ /g, '-')
-          .replace(/[^\w-]+/g, '')
-      }))
+
+      const formData = new FormData()
+
+      console.log(data.ImageArray[0]);
+      
+
+      formData.append("featureImage", data.featureImage)
+      formData.append("description", data.description)
+      formData.append("title", data.title)
+      formData.append("titleslug", data.title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''))
+      formData.append("price", data.price)
+      formData.append("category", data.category)
+      formData.append("ImageArray", data.ImageArray[0])
+      formData.append("featureImage", data.featureImage[0])
+
+      if (!values.edit) {
+
+        dispatch(productActionCreator.createProduct(formData))
+
+      } else {
+
+        let ImageArray = data.ImageArray
+
+        if (!Array.isArray(ImageArray)) {
+          ImageArray = ImageArray.split(',')
+        }
+
+        dispatch(productActionCreator.updateProduct({ data: { ...data, _id: EditProductDetails._id, ImageArray: ImageArray }, history: history }))
+
+      }
 
     } catch (error) {
       console.log(error);
@@ -56,12 +88,10 @@ const AddProduct = () => {
       setValue("title", "")
       setValue("price", 0)
       setValue("category", "")
-      setValue("ImageArray", [])
-      setValue("featureImage", "") 
     }
 
     console.log(values.edit);
-    
+
 
 
   }, [values.edit])
@@ -73,7 +103,7 @@ const AddProduct = () => {
 
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} tw="flex flex-col  min-h-screen max-w-5xl mx-auto p-10 text-center " >
+    <form onSubmit={handleSubmit(onSubmit)} tw="flex flex-col  min-h-screen max-w-5xl mx-auto p-10 text-center " encType="multipart/form-data" >
       <p>Add Product</p>
       <Label>Title</Label>
       <Input {...register("title", { required: "this is Required" })} />
@@ -81,10 +111,6 @@ const AddProduct = () => {
       <Label>Price</Label>
       <Input {...register("price", { required: "this is Required" })} />
       { errors.price && <p>{errors.price.message}</p>}
-
-      <Label>Feature Image</Label>
-      <Input {...register("featureImage", { required: "this is Required" })} />
-      { errors.featureImage && <p>{errors.featureImage.message}</p>}
 
       <Label  >Category</Label>
       <select  {...register("category", { required: "this is Required" })} tw=" text-black border w-40 p-5">
@@ -94,33 +120,40 @@ const AddProduct = () => {
         <option value="must-haves">Must haves</option>
       </select>
 
-      <Label>ImageUrl</Label>
-      <textarea {...register("ImageArray", { required: "this is Required" })} tw="m-5 text-black border text-center focus:(outline-none ring-4)" />
+      <input  {...register("featureImage", { required: "this is Required" })} type="file"  />
+      <input  {...register("ImageArray", { required: "this is Required" })} type="file"  />
+     
 
 
 
-      {/* <div>
-              <label tw="block text-sm font-medium text-gray-700">
-                Cover photo
+
+      {/* <div tw="my-4" >
+        <label tw="block text-sm font-medium">
+          ImageArray
               </label>
-              <div tw="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                <div tw="space-y-1 text-center">
-                  <svg tw="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden=""this is Required"">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                  <div tw="flex text-sm text-gray-600">
-                    <label htmlFor="file-upload" tw="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" tw="sr-only" />
-                    </label>
-                    <p tw="pl-1">or drag and drop</p>
-                  </div>
-                  <p tw="text-xs text-gray-500">
-                    PNG, JPG, GIF up to 10MB
+        <div tw="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+          <div tw="space-y-1 text-center">
+            <svg tw="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden={false} >
+              <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <div tw="flex text-sm">
+              <label tw="relative cursor-pointer rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                <span>Upload a file</span>
+                <input  {...register("ImageArray", { required: "this is Required" })} type="file" tw="sr-only" />
+              </label>
+              <p tw="pl-1">or drag and drop</p>
+            </div>
+            <p tw="text-xs">
+              PNG, JPG, GIF up to 10MB
                   </p>
-                </div>
-              </div>
-            </div> */}
+          </div>
+        </div>
+      </div> */}
+
+
+
+
+
 
       <div tw='text-black'>
         <CKEditor
@@ -137,7 +170,7 @@ const AddProduct = () => {
       { errors.description && <p>{errors.description.message}</p>}
 
 
-      <button type="submit" tw="py-3 px-4 mt-3 inline-block focus:outline-none  text-white text-center bg-indigo-600 hover:bg-indigo-500 rounded-md" >Add Product</button>
+      <button type="submit" tw="py-3 px-4 mt-3 inline-block focus:outline-none  text-white text-center bg-indigo-600 hover:bg-indigo-500 rounded-md" >{values.edit ? 'Update Product' : 'Add Product'}</button>
 
     </form>
   )
