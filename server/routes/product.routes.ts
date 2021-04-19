db2db7568f8f499a0bcd83b08ea332413bb5cae9
aman tiwari <amantiwari78632@gmail.com>
2021-04-20 00:00:31 +0530

    refocring code

--- server/routes/product.routes.ts
+++ server/routes/product.routes.ts
@@ -1,12 +1,10 @@
 import * as productController from "../controllers/product.controller";
 import { Router } from "express";
-import { body, check } from "express-validator";
+import { body } from "express-validator";
 import { use } from "../util/TryCatch";
+import Uploder from "../util/ImageFileUploader";
 import passport from "passport";
-import multer from "multer";
-import path from "path";
-import fs from "fs";
-import { Request, Response } from "express";
+
 
 const updateProductValidations = [
   body("title", "toast.post.title_empty").not().isEmpty(),
@@ -14,66 +12,22 @@ const updateProductValidations = [
 
 const isAuth = passport.authenticate("jwt", { session: false });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    if (!req.body.folder) {
-      req.body.folder = Date.now();
-    }
 
-    const dir = path.join(
-      __dirname,
-      "..",
-      "images",
-      req.body.folder.toString()
-    );
-
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    return cb(null, dir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now().toString() + path.extname(file.originalname));
-  },
-});
-
-const fileFilter = (
-  _: Request,
-  file: Express.Multer.File,
-  cb: multer.FileFilterCallback
-) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    cb(null, false);
-  }
-  cb(null, true);
-};
-
-const upload = multer({ storage: storage, fileFilter: fileFilter });
-
-const fullyUpload = upload.fields([
-  {
-    name: "featureImage",
-    maxCount: 1,
-  },
-  {
-    name: "ImageArray",
-    maxCount: 10,
-  },
-]);
 const product: Router = Router();
 
 product.post(
   "/create",
   isAuth,
-  fullyUpload,
-  updateProductValidations,
+  Uploder,
+  updateProductValidations, 
   use(productController.create)
 );
 
 product.get("/gets", isAuth, use(productController.getProducts));
 product.get("/getsCategories", use(productController.getProductCategory));
 product.get("/get/:titleslug", use(productController.getProduct));
-product.post("/update", isAuth, use(productController.updateProduct));
+product.post("/update/:id", isAuth, Uploder, use(productController.updateProduct));
 product.delete("/delete/:id", isAuth, use(productController.deleteProduct));
+product.delete("/deleteImage/:id/:folder/:filename", isAuth, use(productController.deleteImageProduct));
 
 export default product;
