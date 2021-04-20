07dc7272eb9dfd73a2a42b060567f801fab8bd21
aman tiwari <amantiwari78632@gmail.com>
2021-04-20 23:59:12 +0530

    remove auth in product api

--- server/routes/product.routes.ts
+++ server/routes/product.routes.ts
@@ -21,7 +21,7 @@ product.post(
   use(productController.create)
 );
 
-product.get("/gets", isAuth, use(productController.getProducts));
+product.get("/gets", use(productController.getProducts));
 product.get("/getsCategories", use(productController.getProductCategory));
 product.get("/get/:titleslug", use(productController.getProduct));
 product.post(
