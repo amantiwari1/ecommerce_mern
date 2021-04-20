aec6759de55510ba79d24221ed4dfbf40245a63f
aman tiwari <amantiwari78632@gmail.com>
2021-04-20 23:56:04 +0530

    choose button or linkcomponents

--- client/src/website/components/Dropdown/Dropdown.tsx
+++ client/src/website/components/Dropdown/Dropdown.tsx
@@ -40,10 +40,7 @@ const DropdownTwin = ({
           <button
             onClick={onClick}
             type="button"
-            tw="inline-flex  justify-center w-full rounded-md  shadow-sm px-2 py-2  text-sm font-medium text-white hover:text-blue-400 focus:(outline-none text-blue-400) "
-            id="options-menu"
-            aria-expanded="true"
-            aria-haspopup="true">
+            tw="inline-flex  justify-center w-full rounded-md  shadow-sm px-2 py-2  text-sm font-medium text-white hover:text-blue-400 focus:(outline-none text-blue-400) ">
             {name}
             {isActive ? (
               <AiFillCaretDown tw="ml-1 items-center mt-0.5" />
@@ -56,22 +53,32 @@ const DropdownTwin = ({
         {isActive && (
           <div
             ref={dropdownRef}
-            tw=" z-20 origin-bottom-right absolute -right-4   rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none dark:(bg-darkmode border-blue-50)"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu">
+            tw=" z-20 origin-bottom-right absolute -right-4   rounded-md bg-white ring-1 ring-black ring-opacity-5 focus:outline-none dark:(bg-darkmode border-blue-50)">
             {navitems.map((navitem, index) => (
-              <div tw="py-1" role="none" key={index}>
-                <Link
-                  onClick={() => {
-                    onClick();
-                    onTurnNav();
-                  }}
-                  to={navitem.to}
-                  tw=" block px-4 py-2  whitespace-nowrap text-sm text-gray-700 hover:(bg-blue-700 text-white) dark:(text-white hover:(bg-blue-700 text-white))"
-                  role="menuitem">
-                  {navitem.name}
-                </Link>
+              <div tw="py-1" key={index}>
+                {navitem.isLink ? (
+                  <Link
+                    onClick={() => {
+                      onClick();
+                      onTurnNav();
+                    }}
+                    to={navitem.to}
+                    tw=" block px-4 py-2  whitespace-nowrap text-sm text-gray-700 hover:(bg-blue-700 text-white) dark:(text-white hover:(bg-blue-700 text-white))"
+                    role="menuitem">
+                    {navitem.name}
+                  </Link>
+                ) : (
+                  <button
+                    onClick={() => {
+                      onClick();
+                      onTurnNav();
+                      navitem.onClick();
+                    }}
+                    tw=" block px-4 py-2  whitespace-nowrap text-sm text-gray-700 hover:(bg-blue-700 text-white) dark:(text-white hover:(bg-blue-700 text-white))"
+                    role="menuitem">
+                    {navitem.name}
+                  </button>
+                )}
               </div>
             ))}
           </div>
