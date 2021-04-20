d5ae5aae644e6003ee0c2fc2ca4d8084cb4b3575
aman tiwari <amantiwari78632@gmail.com>
2021-04-20 23:58:39 +0530

    reforcing code

--- server/routes/cart.routes.ts
+++ server/routes/cart.routes.ts
@@ -3,23 +3,12 @@ import {use} from "../util/TryCatch";
 import passport from "passport";
 import * as cartController from "../controllers/cart.controller";
 
+const isAuth = passport.authenticate("jwt", {session: false});
+
 const cart: Router = Router();
 
-cart.get(
-  "/gets",
-  passport.authenticate("jwt", {session: false}),
-  use(cartController.read)
-);
-cart.post(
-  "/create",
-  passport.authenticate("jwt", {session: false}),
-  use(cartController.add)
-);
-cart.post("/upadte/:id", passport.authenticate("jwt", {session: false}));
-cart.post(
-  "/delete/:id",
-  passport.authenticate("jwt", {session: false}),
-  use(cartController.remove)
-);
+cart.get("/gets", isAuth, use(cartController.read));
+cart.post("/create", isAuth, use(cartController.add));
+cart.post("/delete/:id", isAuth, use(cartController.remove));
 
 export default cart;
