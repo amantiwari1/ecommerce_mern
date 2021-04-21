a7348762674837db4f43bed2952ac0b182faf120
aman tiwari <amantiwari78632@gmail.com>
2021-04-21 15:00:09 +0530

    added order slice to store redux

--- client/src/shared/store.ts
+++ client/src/shared/store.ts
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import ProductReducer from "../reducers/ProductSlice";
 import UserSlice from "../reducers/UserSlice";
 import CartSlice from "../reducers/CartSlide";
+import OrderSlide from "../reducers/OrderSlide";
 
 export const store = configureStore({
   reducer: {
     productReducer: ProductReducer,
     userReducer: UserSlice,
     cartReducer: CartSlice,
+    orderReducer: OrderSlide,
   },
 });
 
