46d5c5c346457c93a88e827d61f2724d26affef7
aman tiwari <amantiwari78632@gmail.com>
2021-04-21 15:36:11 +0530

    fixed

--- server/util/secrets.ts
+++ server/util/secrets.ts
@@ -1,30 +1,17 @@
-import dotenv from "dotenv";
 import {
-  HOST_URL_PROD,
   HOST_URL_DEV,
-  SERVER_PORT_PROD,
   SERVER_PORT_DEV,
-  HOST_NAME_PROD,
   HOST_NAME_DEV,
 } from "../../client/src/models/HostUrl";
+import dotenv from "dotenv";
+ 
+dotenv.config()
 
-export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === "production";
 
-if (prod) {
-  console.log = (input: string): void => {
-    /* Disable log in production */
-  };
-  dotenv.config({path: ".env.production"});
-} else {
-  console.log(
-    "  Using .env.development file to supply config environment variables"
-  );
-  dotenv.config({path: ".env.development"}); // you can delete this after you create your own .env file!
-}
-
-export const JWT_SECRET = process.env.JWT_SECRET || "someserver";
+export const JWT_SECRET = process.env.JWT_SECRET || "secret";
 export const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log(MONGODB_URI)
 
 if (!MONGODB_URI) {
   console.error(
@@ -36,15 +23,11 @@ if (!MONGODB_URI) {
 let url: string;
 let server_port: number;
 let origin: string;
-if (prod) {
-  url = HOST_URL_PROD;
-  server_port = SERVER_PORT_PROD;
-  origin = HOST_NAME_PROD;
-} else {
-  url = HOST_URL_DEV;
-  server_port = SERVER_PORT_DEV;
-  origin = HOST_NAME_DEV;
-}
+
+url = HOST_URL_DEV;
+server_port = SERVER_PORT_DEV;
+origin = HOST_NAME_DEV;
+
 export const HOST_URL = url;
 export const SERVER_PORT = server_port;
 export const ORIGIN_URI = origin;
