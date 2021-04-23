1ea511222ae26fefe62c216c1240c3170f5b3a83
aman tiwari <amantiwari78632@gmail.com>
2021-04-23 19:02:20 +0530

    fix two port

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,8 @@ import app from "./app";
 
 const server = app.listen(app.get("server_port"), () => {
   console.log(
-    "  App is running at %s:%d in %s mode",
+    "  App is running at %s in %s mode",
     app.get("origin_uri"),
-    app.get("server_port"),
     app.get("env")
   );
   console.log("  Press CTRL-C to stop\n");
